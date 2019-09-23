e00ee5872ed6c31b9535c8ade9642d710f0f473a
Sa4ras <31968535+Sa4ras@users.noreply.github.com>
2019-09-23 23:09:13 +0300

    Update Lab1_Aitken_Scheme.py

--- Lab1_Aitken_Scheme.py
+++ Lab1_Aitken_Scheme.py
@@ -3,16 +3,12 @@ import numpy as np
 import random
 
 q_of_x = int(input("Input number of X values: "))
-X = random.sample(range(-5,5), q_of_x)
-#X = [-1, 0, 2, 4, 6, 9]
-#Y = [-2, 3, 1, 7, -1, 5]
+X = random.sample(range(-10,10), q_of_x)
 Y = []
-for i in X:
-    Y.append(math.pow(math.e, 7*float(i)**2 + 1))
+for i in range(len(X)):
+    Y.append(math.pow(math.e, 7*X[i]**2 + 1))
 print(X)
 print(Y)
-n = len(X)
-print("Number of elements: ", n)
 x0 = float(input("Input x0: "))
 
 ans_list = []
@@ -24,14 +20,17 @@ def aitken_scheme(X, Y, j):
         aitken.append(round(np.linalg.det(first) / (X[i + 1 + j] - X[i]), 3))
     ans_list.append(aitken[0])
     print(aitken)
-    if len(X) < 2 or len(aitken) == 1:
+    if len(X) <= 2 or len(aitken) == 1:
         return aitken[0]
     else:
         return aitken_scheme(X, aitken, j+1)
 
 aitken_scheme(X,Y,0)
-for i in range(len(ans_list)+1):
-    if abs(ans_list[i+1] - ans_list[i]) < abs(ans_list[i+2] - ans_list[i+1]):
-        print("Aitken Scheme answer: ", ans_list[i+1])
-        break
+if len(X) > 2:
+    for i in range(len(ans_list)):
+        if abs(ans_list[i+1] - ans_list[i]) <= abs(ans_list[i+2] - ans_list[i+1]):
+            print("Aitken Scheme answer: ", ans_list[i+1])
+            break
+else:
+    print("Aitken Scheme answer: ", ans_list[0])
 print("Python function answer: ", math.pow(math.e, 7*x0**2 + 1))
